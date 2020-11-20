02f8a16b3236e27302cf9f5b8cb1f8c17fe07474
yongama <yongamafayo@gmail.com>
2020-11-20 12:26:24 +0200

    heroku commit

--- index.js
+++ index.js
@@ -7,13 +7,13 @@ const bodyParser = require('body-parser')
 const app = express();
 const pg = require("pg");
 const Pool = pg.Pool;
-const connectionString = process.env.DATABASE_URL || 'postgresql://yongama:pg123@localhost:5432/waiters';
+const connectionString = process.env.DATABASE_URL || 'postgresql://yongama:pg123@localhost:5432/waiterz';
 const pool = new Pool({
     connectionString
 });
 
-const WaitersApp = require("./waiter-function")
-const waitersApp = WaitersApp()
+const WaitersApp = require("./waiter")
+const waiter = WaitersApp(pool)
 
 const session = require('express-session')
 const flash = require('express-flash')
@@ -41,8 +41,123 @@ app.post("/", async function (req, res) {
 })
 
 app.get("/", async function (req, res) {
-    res.render("index")
+    res.render("signin")
 })
+
+app.get('/logout', (req, res) => {
+    res.render('signin');
+});
+
+app.post('/signin', async (req, res, next) => {
+    const { job_Type, signinUsername } = req.body;
+    let username = signinUsername;
+    try {
+        await logIn(username, job_Type, req, res);
+    } catch (error) {
+        next(error);
+    }
+});
+
+const logIn = async (username, job_Type, req, res) => {
+    let found = await waiter.findUser(username, job_Type);
+
+    if (found === 'waiter') {
+        req.session.user_name = username;
+        res.redirect('/waiters/' + username);
+    } else if (found === 'Admin') {
+        res.redirect('days');
+    } else {
+        req.flash('error', 'incorrect! please enter correct details');
+        res.redirect('/');
+    }
+}
+
+app.get('/waiters/:username', async (req, res, next) => {
+    try {
+        let username = req.params.username;
+        let findUser = await waiter.findusername(username);
+        let weekdays = await waiter.getWeekdays(username);
+        res.render('home', {
+            daynames: weekdays,
+            username,
+            findUser
+        });
+    } catch (error) {
+        next(error);
+    }
+})
+
+
+app.post('/waiters/:username', async (req, res, next) => {
+    try {
+        let username = req.params.username;
+        let weekdays = await waiter.getWeekdays(username);
+        if (weekdays != undefined || weekdays != [] &&
+            username != undefined || username != "") {
+            let shift = {
+                username: username,
+                days: Array.isArray(req.body.dayname) ? req.body.dayname : [req.body.dayname]
+            }
+            req.flash('info', 'Shift(s) succesfully added');
+            await waiter.dayShift(shift);
+            res.redirect(`/waiters/${username}`);
+        }
+
+    } catch (error) {
+        next(error);
+    }
+})
+
+app.get('/days', async (req, res, next) => {
+    try {
+        await waiter.getWeekdays();
+        let storedShifts = await waiter.groupByDay();
+        res.render('days', {
+            storedShifts
+        });
+    } catch (error) {
+        next(error);
+    }
+})
+
+app.get('/clear', async (req, res, next) => {
+    try {
+        await waiter.clearShifts();
+        req.flash('info', 'Shift succesfully deleted');
+        res.redirect('days');
+    } catch (error) {
+        next(error)
+    }
+})
+
+app.get('/signup', async (req, res, next) => {
+    try {
+        res.render('signup');
+    } catch (e) {
+        next(e);
+    }
+})
+
+app.post('/signup', async (req, res, next) => {
+    try {
+        const { full_name, username, job_Type } = req.body;
+        if (full_name !== undefined && username !== undefined
+            && job_Type !== undefined && job_Type !== '') {
+            if (await waiter.addWaiter(full_name, username, job_Type)) {
+                req.flash('info', 'Succesfully registered');
+            } else {
+                req.flash('error', 'incorrect details');
+            }
+        } else {
+            req.flash('error', 'Fill in the required information');
+        }
+
+        await logIn(username, job_Type, req, res);
+    } catch (e) {
+        next(e);
+    }
+});
+
 
 app.get("/waiters", async function (req, res) {
     const waiters = await waitersApp.getWaiters()
@@ -90,7 +205,7 @@ app.get("/reset", async function (req, res) {
     res.render("days")
 })
 
-const PORT = process.env.PORT || 3021;
+const PORT = process.env.PORT || 3008;
 
 app.listen(PORT, function () {
     console.log("App started at port:", PORT)
--- table.sql
+++ table.sql
@@ -1,22 +1,82 @@
-create table waiters(
-    id serial primary key,
-    waiters text not null
+drop table if exists dayShifts, waiterDB, weekdays;
+
+create table waiterDB(
+id serial not null primary key,
+username text not null,
+full_name text not null,
+position text not null
 );
 
-create table weekdays(
-    id serial primary key,
-    weekdays text not null
+CREATE TABLE weekdays(
+ id serial not null PRIMARY KEY ,
+  dayName  VARCHAR(20) UNIQUE 
 );
 
-create table shifts(
-    weekdays_name text not null,
-    waiters_name text not null
+CREATE TABLE dayShifts(
+id serial not NULL PRIMARY key,
+waiter_id  int not null,
+weekday_id int not null ,
+foreign key (waiter_id) references waiterDB(id),
+foreign key (weekday_id) references weekdays(id)
 );
 
-insert into weekdays (weekdays) values ('sunday');
-insert into weekdays (weekdays) values ('monday');
-insert into weekdays (weekdays) values ('tuesday');
-insert into weekdays (weekdays) values ('wednesday');
-insert into weekdays (weekdays) values ('thursday');
-insert into weekdays (weekdays) values ('friday');
-insert into weekdays (weekdays) values ('saturday');
+-- --  INSERT data into waiter
+
+INSERT INTO waiterDB (username,full_name,position) VALUES ('Stylez','Yongama','waiter');
+INSERT INTO waiterDB (username,full_name,position) VALUES ('General','Avongile','waiter');
+INSERT INTO waiterDB (username,full_name,position) VALUES ('Sheriff','Vusi','waiter');
+INSERT INTO waiterDB (username,full_name,position) VALUES ('Ngamla','Akhona','waiter');
+
+
+-- -- INSERT WEEKDAYS
+INSERT INTO weekdays (dayName) VALUES ('Sunday');
+INSERT INTO weekdays (dayName) VALUES ('Monday');
+INSERT INTO weekdays (dayName) VALUES ('Tuesday');
+INSERT INTO weekdays (dayName) VALUES ('Wednesday');
+INSERT INTO weekdays (dayName) VALUES ('Thursday');
+INSERT INTO weekdays (dayName) VALUES ('Friday');
+INSERT INTO weekdays (dayName) VALUES ('Saturday');
+
+-- selcting a shift
+INSERT INTO dayShifts (waiter_id,weekday_id) VALUES(1,1);
+INSERT INTO dayShifts (waiter_id,weekday_id) VALUES(1,2);
+INSERT INTO dayShifts (waiter_id,weekday_id) VALUES(1,3);
+INSERT INTO dayShifts (waiter_id,weekday_id) VALUES(1,7);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- create table waiters(
+--     id serial primary key,
+--     waiters text not null
+-- );
+
+-- create table weekdays(
+--     id serial primary key,
+--     weekdays text not null
+-- );
+
+-- create table shifts(
+--     weekdays_name text not null,
+--     waiters_name text not null
+-- );
+
+-- insert into weekdays (weekdays) values ('sunday');
+-- insert into weekdays (weekdays) values ('monday');
+-- insert into weekdays (weekdays) values ('tuesday');
+-- insert into weekdays (weekdays) values ('wednesday');
+-- insert into weekdays (weekdays) values ('thursday');
+-- insert into weekdays (weekdays) values ('friday');
+-- insert into weekdays (weekdays) values ('saturday');
